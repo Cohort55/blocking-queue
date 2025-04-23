f95e9717f2c378c0daca013a8f8bb104938e6092
edward <edwardlfish@gmail.com>
2025-04-23 10:46:32 +0300

    Implements BlkQueueImpl

--- src/ait/mediation/BlkQueueImpl.java
+++ src/ait/mediation/BlkQueueImpl.java
@@ -1,20 +1,39 @@
 package ait.mediation;
 
+import java.util.LinkedList;
+
 public class BlkQueueImpl<T> implements BlkQueue<T> {
+    private LinkedList<T> queue = new LinkedList<>();
+    private int maxSize;
+
     public BlkQueueImpl(int maxSize) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+        this.maxSize = maxSize;
     }
 
     @Override
-    public void push(T message) {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized void push(T message) {
+        while (queue.size() >= maxSize) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        queue.add(message);
+        notify();
     }
 
     @Override
-    public T pop() {
-        // TODO
-        throw new UnsupportedOperationException("Not implemented");
+    public synchronized T pop() {
+        while (queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        T message = queue.poll();
+        notifyAll();
+        return message;
     }
 }
